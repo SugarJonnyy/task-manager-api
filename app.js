3bf6ccc9996090c0fd09e6cc58d0024affb27d04
Saswata Das <220040079+SugarJonnyy@users.noreply.github.com>
2026-03-24 14:40:59 +0530

    setup initial db

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const tasksRouter = require('./router/tasks')
-
+app.use(express.json()) // json parsing middleware
 //router
 app.get('/', (req, res)=>{
     res.send("task-manager-api")    
---
+++ db/connect.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+
+const connectionString =
+  "mongodb+srv://saswatadas705_db_user:<db_password>@cluster0.xrwgsaw.mongodb.net/?appName=Cluster0";
